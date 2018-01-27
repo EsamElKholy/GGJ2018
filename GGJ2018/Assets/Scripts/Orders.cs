33b2055bd3ca27a9acc42209cc30b1082ac5a5cd
kaisama <esam.k.elkholy@gmail.com>
2018-01-27 12:49:10 +0200

    scene and some scripts

---
+++ GGJ2018/Assets/Scripts/Orders.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orders : MonoBehaviour
+{
+    public void DoOrder()
+    {
+
+    }
+
+    void Effect1()
+    {
+
+    }
+
+    void Effect2()
+    {
+
+    }
+}
